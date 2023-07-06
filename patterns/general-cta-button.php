75b77ce120276592d83afb853fa34d8fb5f293bc
mocanunicoleta <mocanunicoleta.c@gmail.com>
2023-07-06 19:58:26 +0300

    made some patterns

--- patterns/general-cta-button.php
+++ patterns/general-cta-button.php
@@ -5,13 +5,24 @@
  * Categories: call-to-action
  */
 ?>
-<!-- wp:group {"align":"wide","style":{"border":{"width":"1px"},"spacing":{"padding":{"top":"var:preset|spacing|large","right":"var:preset|spacing|large","bottom":"var:preset|spacing|large","left":"var:preset|spacing|large"}}},"borderColor":"contrast","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
-<div class="wp-block-group alignwide has-border-color has-contrast-border-color" style="border-width:1px;padding-top:var(--wp--preset--spacing--large);padding-right:var(--wp--preset--spacing--large);padding-bottom:var(--wp--preset--spacing--large);padding-left:var(--wp--preset--spacing--large)"><!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.5"},"layout":{"selfStretch":"fixed","flexSize":"720px"}},"fontSize":"large"} -->
-<p class="has-large-font-size" style="line-height:1.5"><?php echo esc_html__( 'With its clean, minimal design and powerful feature set, Frost enables agencies to build stylish and sophisticated WordPress websites.', 'frost' ); ?></p>
+<!-- wp:group {"animationsForBlocks":{"animation":"","variation":"fade","mirror":true,"once":false},"align":"full","style":{"spacing":{"padding":{"top":"80px","bottom":"80px"},"margin":{"top":"0","bottom":"0"}}},"backgroundColor":"Blue2","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-blue-2-background-color has-background" id="register-summit" style="margin-top:0;margin-bottom:0;padding-top:80px;padding-bottom:80px"><!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"layout":{"type":"constrained","contentSize":"666px"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","animationsForBlocks":{"animation":"fade","variation":"fade","mirror":false,"once":true},"textColor":"base"} -->
+<h2 class="wp-block-heading has-text-align-center has-base-color has-text-color" data-aos="fade" data-aos-once="true">Register for Summit</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","animationsForBlocks":{"animation":"fade","variation":"fade","delay":450,"mirror":false,"once":true},"style":{"spacing":{"padding":{"right":"40px","left":"40px"}}},"textColor":"base","fontSize":"large"} -->
+<p class="has-text-align-center has-base-color has-text-color has-large-font-size" style="padding-right:40px;padding-left:40px" data-aos="fade" data-aos-delay="450" data-aos-once="true">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dignissim lectus congue, malesuada justo ac, scelerisque lacus. Donec magna ligula</p>
 <!-- /wp:paragraph -->
-<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"right"}} -->
-<div class="wp-block-buttons"><!-- wp:button {"fontSize":"small"} -->
-<div class="wp-block-button has-custom-font-size has-small-font-size"><a class="wp-block-button__link wp-element-button"><?php echo esc_html__( 'Get Started Today', 'frost' ); ?></a></div>
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"animationsForBlocks":{"animation":"fade","variation":"fade","delay":950,"mirror":false,"once":true}} -->
+<div class="wp-block-button" data-aos="fade" data-aos-delay="950" data-aos-once="true"><a class="wp-block-button__link wp-element-button">Register</a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
 <!-- /wp:group -->
---
+++ patterns/general-featured-boxes.php
@@ -0,0 +1,162 @@
+<?php
+/**
+ * Title: Boxes with Icon, heading and text.
+ * Slug: frost/general-featured-boxes
+ * Categories: featured
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"backgroundColor":"orange2","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-orange-2-background-color has-background" style="margin-top:0;margin-bottom:0"><!-- wp:spacer {"height":"120px"} -->
+<div style="height:120px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">Title <mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-secondary-color">Section</mark></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","textColor":"secondary"} -->
+<p class="has-text-align-center has-secondary-color has-text-color">Donec et erat sagittis diam sodales suscipit. Nam sit amet turpis enim. Sed eget lacus vitae dolor</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:columns {"align":"wide","style":{"spacing":{"padding":{"right":"var:preset|spacing|x-small","left":"var:preset|spacing|x-small"}}},"className":"is-style-border-rounded"} -->
+<div class="wp-block-columns alignwide is-style-border-rounded" style="padding-right:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--x-small)"><!-- wp:column {"style":{"spacing":{"padding":{"top":"39px","right":"24px","bottom":"40px","left":"24px"},"blockGap":"0"}},"backgroundColor":"base"} -->
+<div class="wp-block-column has-base-background-color has-background" style="padding-top:39px;padding-right:24px;padding-bottom:40px;padding-left:24px"><!-- wp:image {"align":"center","id":29,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/people.svg'; ?>" alt="" class="wp-image-29"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:spacer {"height":"24px"} -->
+<div style="height:24px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"textColor":"contrast"} -->
+<h3 class="wp-block-heading has-text-align-center has-contrast-color has-text-color">Live Chat</h3>
+<!-- /wp:heading -->
+
+<!-- wp:spacer {"height":"16px"} -->
+<div style="height:16px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Donec et erat sagittis diam sodales suscipit. Nam sit amet turpis enim. Sed eget lacus vitae dolor</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"padding":{"top":"39px","right":"24px","bottom":"40px","left":"24px"},"blockGap":"0"}},"backgroundColor":"base"} -->
+<div class="wp-block-column has-base-background-color has-background" style="padding-top:39px;padding-right:24px;padding-bottom:40px;padding-left:24px"><!-- wp:image {"align":"center","id":28,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/quality.svg'; ?>" alt="" class="wp-image-28"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:spacer {"height":"24px"} -->
+<div style="height:24px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"textColor":"contrast"} -->
+<h3 class="wp-block-heading has-text-align-center has-contrast-color has-text-color">Quality</h3>
+<!-- /wp:heading -->
+
+<!-- wp:spacer {"height":"16px"} -->
+<div style="height:16px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Donec et erat sagittis diam sodales suscipit. Nam sit amet turpis enim. Sed eget lacus vitae dolor</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"padding":{"top":"39px","right":"24px","bottom":"40px","left":"24px"},"blockGap":"0"}},"backgroundColor":"base"} -->
+<div class="wp-block-column has-base-background-color has-background" style="padding-top:39px;padding-right:24px;padding-bottom:40px;padding-left:24px"><!-- wp:image {"align":"center","id":27,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/star.svg'; ?>" alt="" class="wp-image-27"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:spacer {"height":"24px"} -->
+<div style="height:24px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"textColor":"contrast"} -->
+<h3 class="wp-block-heading has-text-align-center has-contrast-color has-text-color">Exclusive Offers</h3>
+<!-- /wp:heading -->
+
+<!-- wp:spacer {"height":"16px"} -->
+<div style="height:16px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Donec et erat sagittis diam sodales suscipit. Nam sit amet turpis enim. Sed eget lacus vitae dolor</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"align":"wide","style":{"spacing":{"padding":{"right":"var:preset|spacing|x-small","left":"var:preset|spacing|x-small"}}},"className":"is-style-border-rounded"} -->
+<div class="wp-block-columns alignwide is-style-border-rounded" style="padding-right:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--x-small)"><!-- wp:column {"style":{"spacing":{"padding":{"top":"39px","right":"24px","bottom":"40px","left":"24px"},"blockGap":"0"}},"backgroundColor":"base"} -->
+<div class="wp-block-column has-base-background-color has-background" style="padding-top:39px;padding-right:24px;padding-bottom:40px;padding-left:24px"><!-- wp:image {"align":"center","id":29,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/certificate.svg'; ?>" alt="" class="wp-image-29"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:spacer {"height":"24px"} -->
+<div style="height:24px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"textColor":"contrast"} -->
+<h3 class="wp-block-heading has-text-align-center has-contrast-color has-text-color">Certificate</h3>
+<!-- /wp:heading -->
+
+<!-- wp:spacer {"height":"16px"} -->
+<div style="height:16px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Donec et erat sagittis diam sodales suscipit. Nam sit amet turpis enim. Sed eget lacus vitae dolor</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"padding":{"top":"39px","right":"24px","bottom":"40px","left":"24px"},"blockGap":"0"}},"backgroundColor":"base"} -->
+<div class="wp-block-column has-base-background-color has-background" style="padding-top:39px;padding-right:24px;padding-bottom:40px;padding-left:24px"><!-- wp:image {"align":"center","id":28,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/video.svg'; ?>" alt="" class="wp-image-28"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:spacer {"height":"24px"} -->
+<div style="height:24px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"textColor":"contrast"} -->
+<h3 class="wp-block-heading has-text-align-center has-contrast-color has-text-color">20h Video</h3>
+<!-- /wp:heading -->
+
+<!-- wp:spacer {"height":"16px"} -->
+<div style="height:16px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Donec et erat sagittis diam sodales suscipit. Nam sit amet turpis enim. Sed eget lacus vitae dolor</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"padding":{"top":"39px","right":"24px","bottom":"40px","left":"24px"},"blockGap":"0"}},"backgroundColor":"base"} -->
+<div class="wp-block-column has-base-background-color has-background" style="padding-top:39px;padding-right:24px;padding-bottom:40px;padding-left:24px"><!-- wp:image {"align":"center","id":27,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full"><img src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/assets.svg'; ?>" alt="" class="wp-image-27"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:spacer {"height":"24px"} -->
+<div style="height:24px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"textColor":"contrast"} -->
+<h3 class="wp-block-heading has-text-align-center has-contrast-color has-text-color">Assets</h3>
+<!-- /wp:heading -->
+
+<!-- wp:spacer {"height":"16px"} -->
+<div style="height:16px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Donec et erat sagittis diam sodales suscipit. Nam sit amet turpis enim. Sed eget lacus vitae dolor</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer {"height":"120px"} -->
+<div style="height:120px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group -->
