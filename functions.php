fba984e8679175b8fe9c392778c475056191798d
mocanunicoleta <mocanunicoleta.c@gmail.com>
2023-08-15 14:28:24 +0300

    qa

--- functions.php
+++ functions.php
@@ -243,52 +243,54 @@ fa_custom_setup_cdn_webfont(
  * 
  */
 function exclude_current_post_shortcode( $atts ) {
-    global $post;
-    $current_post_id = $post->ID;
+  global $post;
+  $current_post_id = $post->ID;
 
-    $atts = shortcode_atts( array(
-        'posts_per_page' => 5,
-        'post_type'      => 'post',
-    ), $atts, 'exclude_current_post' );
+  $atts = shortcode_atts( array(
+      'posts_per_page' => 3,
+      'post_type'      => 'post',
+  ), $atts, 'exclude_current_post' );
 
-    $query_args = array(
-        'post_type'      => $atts['post_type'],
-        'posts_per_page' => absint( $atts['posts_per_page'] ),
-        'post__not_in'   => array( $current_post_id ),
-    );
+  $query_args = array(
+      'post_type'      => $atts['post_type'],
+      'posts_per_page' => absint( $atts['posts_per_page'] ),
+      'post__not_in'   => array( $current_post_id ),
+  );
 
-    $query = new WP_Query( $query_args );
+  $query = new WP_Query( $query_args );
 
-    if ( $query->have_posts() ) {
-        ob_start();?>
-<ul class="query-except-current-post">
-       <?php while ( $query->have_posts() ) {
-            $query->the_post();
-            ?>
-            <li class="custom-post-item">
-                <?php if ( has_post_thumbnail() ) : ?>
-                    <figure class="custom-post-thumbnail">
-                        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
-										</figure>
-                <?php endif; ?>
+  if ( $query->have_posts() ) {
+  ob_start();?>
+  <ul class="query-except-current-post">
+   <?php while ( $query->have_posts() ) {
+      $query->the_post();
+      ?>
+      <li class="custom-post-item">
+        <a href="<?php echo get_permalink(); ?>" target="_self">
+          <?php if ( has_post_thumbnail() ) : ?>
+              <figure class="custom-post-thumbnail">
+                  <?php the_post_thumbnail(); ?>
+							</figure>
+          <?php endif; ?>
 
-                <div class="custom-post-details">
-                    <div class="custom-post-date">
-                        <?php echo get_the_date('m.Y'); ?>
-                    </div>
-                    <h4 class="custom-post-title">
-                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                    </h4>
-                </div>
-								</li>
-            <?php
-        }?>
-				</ul><?php
+          <div class="custom-post-details">
+              <div class="custom-post-date">
+                  <?php echo get_the_date('m.Y'); ?>
+              </div>
+              <h4 class="custom-post-title">
+                  <?php the_title(); ?>
+              </h4>
+          </div>
+        </a>
+			</li><?php
+    }?>
+		</ul><?php
+  wp_reset_postdata();
+  $shortcode_output = ob_get_clean();
 
-        wp_reset_postdata();
-
-        return ob_get_clean();
-    }
+  // Sanitize the shortcode output to prevent empty <p> tags
+  return do_shortcode( $shortcode_output );
+  }
 }
 add_shortcode( 'exclude_current_post', 'exclude_current_post_shortcode' );
 
